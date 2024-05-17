0962ef9d913f2263e8044f01ed15c5ea3d2cabc0
ojas <ojasacharekar257@gmail.com>
2024-05-17 22:49:50 +0530

    added the configurations and Database

--- api_integration_proj/settings_rail.py
+++ api_integration_proj/settings_rail.py
@@ -2,3 +2,19 @@ from api_integration_proj.settings import *
 from decouple import config
 
 SECRET_KEY = config('SECRET_KEY')
+
+ALLOWED_HOSTS = ['web-production-f0e9.up.railway.app']
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "USER": config('DATABASE_USER'),
+        "PASSWORD": config('DATABASE_PASSWORD'),
+        "HOST": config('DATABASE_HOST'),
+        "PORT": config('DATABASE_PORT'),
+        "NAME": config('DATABASE_NAME'),
+        "OPTIONS": {
+            "sslmode": "require"
+        },
+    }
+}
